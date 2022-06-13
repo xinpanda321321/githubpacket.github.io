ced348ea8405d35c8e303b42a24bdb12da29666f
xinpanda321321 <56233565+xinpanda321321@users.noreply.github.com>
2022-06-13 09:43:36 +0800

    update table
    update table

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ function loadJson() {
 
 	if(response.status == 200) {
 		var data = response.data
-		console.log(data);
-		document.getElementById("school").innerHTML = "School : " + data["School"];
-		document.getElementById("program").innerHTML = "Program/Major : " + data["Program"];
-		document.getElementById("type").innerHTML = "Type : " + data["Type"];
-		document.getElementById("year").innerHTML = "Year : " + data["Year"];
+		var table = document.getElementById("myTable");
+		var row = table.insertRow(0);
+		var cel1 = row.insertCell(0);
+		var cel2 = row.insertCell(1);
+		var cel3 = row.insertCell(2);
+		var cel4 = row.insertCell(3);
+		cel1.innerHTML = data["School"];
+		cel2.innerHTML = data["Program"];
+		cel3.innerHTML = data["Type"];
+		cel4.innerHTML = data["Year"];
 	}
 }
